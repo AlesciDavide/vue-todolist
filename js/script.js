49146410a3019c967296f4b5518b4af8e63f0661
AlesciDavide <davi.alesci@email.com>
2024-05-30 15:06:32 +0200

    aggiunto button e toggle della classe done

--- js/script.js
+++ js/script.js
@@ -20,13 +20,34 @@ createApp({
                     text: 'fare una capriola',
                     done: false,
                 },
-            ]
+            ],
+            newTask: '',
         }
     },
     methods:{
-        /* funzione per rimuovere un item dalla lista */
+        /* funzione per rimuovere un task dalla lista */
         removeItemTodo: function(index){
             this.todoList.splice(index, 1);
-        }
+        },
+        /* funzione per aggiungere un task alla lista */
+        addItemTodo: function(newTask){
+            if(newTask.trim().length > 0) {
+                const newObj = {
+                    text: newTask.trim(),
+                    done: false
+                }
+                this.todoList.push(newObj);
+            }
+            this.clearInput();
+        },
+        /* pulire l'input */
+        clearInput: function(){
+            this.newTask = '';
+        },
+        /* toggle classe done */
+        toggClassDone: function(index){
+            this.todoList[index].done = !this.todoList[index].done
+        },
+        
     }
 }).mount('#app')
